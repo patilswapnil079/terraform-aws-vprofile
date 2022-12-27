eebde2953a45bc2277db93260238f400b9cd00ff
swapnilpatil <patil.swapnil079@gmail.com>
2022-12-27 12:05:17 +0530

    key pair

---
+++ var.tf
@@ -0,0 +1,96 @@
+variable AWS_REGION {
+    default = "us-east-1"
+}
+
+variable AMIS {
+    type = map
+    default = {
+        us-east-1 = "ami-0fe472d8a85bc7b0e"
+
+    }
+}
+
+variable PRIV_KEY_PATH {
+    default = "vprofilekey"
+}
+
+variable PUB_KEY_PATH {
+    default = "vprofilekey.pub"
+}
+
+variable USERNAME {
+    default = "ubuntu"
+}
+
+variable MYIP {
+    default = "cidr_block"
+}
+
+variable rmquser {
+    default = "rabbitmq"
+}
+
+variable rmqpass {
+    default = "Gr33n@pple123456"
+}
+
+variable dbuser {
+    default = "admin"
+}
+
+variable pass {
+    default = "admin123"
+}
+
+variable dbname {
+    default = "accounts"
+}
+
+variable instance_count {
+    default = "1"
+}
+
+variable VPC_NAME {
+    default = "vprofile-VPC"
+}
+
+variable ZONE1 {
+    default = "us-east-1a"
+}
+
+variable ZONE2 {
+    default = "us-east-1b"
+}
+
+variable ZONE3 {
+    default = "us-east-1c"
+}
+
+variable VpcCIDR {
+    default = "172.21.0.0/16"
+}
+
+variable PubSub1CIDR {
+    default = "172.21.1.0/24"
+}
+
+variable PubSub2CIDR {
+    default = "172.21.2.0/24"
+}
+
+variable PubSub3CIDR {
+    default = "172.21.3.0/24"
+}
+
+variable PrivSub1CIDR {
+    default = "172.21.4.0/24"
+}
+
+variable PrivSub2CIDR {
+    default = "172.21.5.0/24"
+}
+
+variable PrivSub3CIDR {
+    default = "172.21.6.0/24"
+}
+
