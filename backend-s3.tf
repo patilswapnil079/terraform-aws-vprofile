7c4ce5a0edb594d845ad5b1b6315c0c5b34798b4
swapnilpatil <patil.swapnil079@gmail.com>
2022-12-27 12:28:39 +0530

    vpc file

--- backend-s3.tf
+++ backend-s3.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend "s3" {
-        bucket = "terra-vprofile-state-079"
-        key = "terraform/backend"
-        region = "us-east-1"
-    }
+  backend "s3" {
+    bucket = "terra-vprofile-state-079"
+    key    = "terraform/backend"
+    region = "us-east-1"
+  }
 }
---
+++ vpc.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source = "terraform-aws-module/vpc/aws"
+  name = var.VPC_NAME
+  cidr = var.VpcCIDR
+  azs = [var.ZONE1, var.ZONE2, var.ZONE3]
+  private_subnet = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
+  public_subnet = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    Terraform ="true"
+    Environment = "prod"
+
+  }
+  vpc_tags = {
+      Name = var.VPC_NAME
+
+  }
+}
