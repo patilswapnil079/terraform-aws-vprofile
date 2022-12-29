8a7f225154203c14de8b29cb2859393ad85c718d
swapnilpatil <patil.swapnil079@gmail.com>
2022-12-29 09:32:33 +0530

    security group

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 module "vpc" {
-  source         = "terraform-aws-modules/vpc/aws"
-  name           = var.VPC_NAME
-  cidr           = var.VpcCIDR
-  azs            = [var.ZONE1, var.ZONE2, var.ZONE3]
+  source          = "terraform-aws-modules/vpc/aws"
+  name            = var.VPC_NAME
+  cidr            = var.VpcCIDR
+  azs             = [var.ZONE1, var.ZONE2, var.ZONE3]
   private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
   public_subnets  = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
 
